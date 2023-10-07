3584d8ff6cda4fed46ff874381fc78e90cc9afa4
秋秋 <973146586@qq.com>
2023-10-07 17:18:55 +0700

    add all

---
+++ components/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+import MyFooter from '../Footer';
+function Home() {
+  return (
+    <div>
+      <div className="h-screen justify-center flex">
+      xxxxxxxx (intro to WBF mechanism)
+      </div>
+      {/* <MyFooter></MyFooter> */}
+    </div>
+  );
+}
+
+export default Home;
